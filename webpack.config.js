7b36ca1345276246a67d64f9b40597d4aa6d1470
papi18011998 <papaibrahima98@gmail.com>
2021-12-22 11:47:54 +0000

    mise en place de l'arborescence du projet

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path")
+module.exports = {
+    entry:"./src/app.js",
+    watch:true,
+    output:{
+        filename: "app.js",
+        path: path.resolve(__dirname,"dist")
+    }
+}
