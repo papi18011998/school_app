5f4e362712767e6c3d0b7c9c7999e44295ef25d4
papi18011998 <papaibrahima98@gmail.com>
2021-12-26 19:51:32 +0000

    feat:modification de la carte de l'apprenant

--- src/app.js
+++ src/app.js
@@ -83,17 +83,21 @@ formAdd.addEventListener('submit',(e)=>{
         return
     }
     if (isNaN(valeurMaquette) || valeurMaquette < 0 || valeurMaquette >10 || valeurMaquette=="") {
+        valeurMaquette.classList.add("invalid")
         erreurMaquettage.style.display ="block" 
         return
     }
     if (isNaN(valeurInterface) || valeurInterface < 0 || valeurInterface >10 || valeurInterface=="") {
+        valeurInterface.classList.add("invalid")
         erreurInterface.style.display ="block" 
         return
     }
     if (isNaN(valeurBdd) || valeurBdd < 0 || valeurBdd >10 || valeurBdd=="") {
+        valeurBdd.classList.add("invalid")
         erreurBdd.style.display ="block" 
     }
     if (isNaN(valeurBackend) || valeurBackend < 0 || valeurBackend >10 || valeurBackend=="") {
+        valeurBackend.classList.add("invalid")
         erreurBackend.style.display ="block"
         return 
     }
@@ -116,54 +120,186 @@ formAdd.addEventListener('submit',(e)=>{
     APPRENANTS.push(newApprenant)
     createCard(newApprenant)
     // RESET INPUTS
-    // resetInputs()
+    resetInputs()
 
 })
+// FONCTION DE VERFICATION DES DONNEES
+function checkInputs() {
+    
+}
 // FONCTION DE CREATION DE CARTE DES APPRENANTS
 function createCard(apprenant) {
 let idUpdate = "update-"+apprenant.index
 let idDelete = "delete-"+apprenant.index
-console.log(idUpdate,idDelete);
+let card = "card-"+apprenant.index
+let idPrenom = "prenom"+apprenant.index
+let idNom = "nom"+apprenant.index
+let idNiveau = "niveau"+apprenant.index
+let idBio = "bio"+apprenant.index
+let idMaquette = "maquette"+apprenant.index
+let idInterface = "interface"+apprenant.index
+let idBdd = "bdd"+apprenant.index
+let idBackend = "backend"+apprenant.index
+let modifierApprenant = "modifier"+apprenant.index
+let idModifierApprenantForm = "form"+apprenant.index
+let libelleNiveau = "niveau"+apprenant.index
 liste.insertAdjacentHTML('afterbegin',`
-<div class="card col-11 ps-5 shadow">
-  <div class="row">
-    <div class="text-end">
-      <i class="pe-3 bi bi-pen modifierIcon" id="${idUpdate}" title="Modifier"></i>
-      <i class="pe-3 bi bi-trash deleteIcon" id="${idDelete}" title="Supprimer"></i>
+<div class="card mb-2" id="${card}">
+<div class="row no-gutters">
+    <div class="col-md-5">
+        <div class="text-end bg-light">
+          <i class="pe-3 bi bi-pen modifierIcon" id="${idUpdate}" title="Modifier"></i>
+          <i class="pe-3 bi bi-trash deleteIcon" id="${idDelete}" title="Supprimer"></i>
+        </div>
+        <img src="asset/landing_p4.jpg"  class="avatar" alt="Avatar">
     </div>
-        <div class="col-4 mt-4" style="width:100px;height:100px;">
-          <img src="asset/star.svg" class="avatar rounded-circle" alt="Avatar">
-        </div>
-        <div class="col-8">
-            <div class="card-body">
-              <h5 class="card-title ">${apprenant.prenom} ${apprenant.nom}</h5>
-              <p class="card-text">${apprenant.bio}</p>
-              <p class="card-text"><small class="text-muted">${apprenant.niveau}</small></p>
+    <div class="col-md-7">
+        <div class="card-body">
+          <form id="${idModifierApprenantForm}">
+            <div class="card-title h5 row no-gutters">
+                <!-- ====================================CHAMP PRENOM==================================== -->
+                <div class="col-sm-12 col-md-6 col-lg-6 mb-2" >
+                  <input type="text" id="${idPrenom}" class="form-control" style="border: none;" value="${apprenant.prenom}" disabled>
+                </div>
+                <!-- ====================================CHAMP NOM==================================== -->
+                <div class="col-sm-12 col-md-6 col-lg-6 mb-2">
+                  <input type="text" id="${idNom}" class="form-control" style="border: none;" value="${apprenant.nom}" disabled>
+                </div>
+                <!-- ====================================CHAMP NIVEAU==================================== -->
+                <div class="col-12 mb-2">
+                  <select class="form-select" aria-label="Default select example" style="border: none;" id="${idNiveau}" disabled>
+                    <option value="Débutant">Débutant</option>
+                    <option value="Intermédiaire">Intermédiaire</option>
+                    <option value="Expert">Expert</option>
+                  </select>
+                </div>
+                <!-- ====================================CHAMP BIO==================================== -->
+                <div class="form-group mb-2">
+                  <textarea class="form-control" id="${idBio}" rows="1" style="border: none;" disabled>${apprenant.bio}</textarea>
+                </div>
+                <!-- ====================================CHAMP MAQUETTE==================================== -->
+                <div class="col-sm-12 col-md-6 col-lg-6 mb-2">
+                  <label for="${idMaquette}">Maquette:</label>
+                  <input type="number" id="${idMaquette}" class="form-control" style="border: none;" value="${apprenant.maquettage}" disabled>
+                </div>
+                <!-- ====================================CHAMP INTERFACE==================================== -->
+                <div class="col-sm-12 col-md-6 col-lg-6 mb-2">
+                  <label for="${idInterface}">Interface:</label>
+                  <input type="number" id="${idInterface}" class="form-control" style="border: none;" value="${apprenant.interface}" disabled>
+                </div>
+                <!-- ====================================CHAMP BDD==================================== -->
+                <div class="col-sm-12 col-md-6 col-lg-6 mb-2">
+                  <label for="${idBdd}">Base de données:</label>
+                  <input type="number" id="${idBdd}" class="form-control" style="border: none;" value="${apprenant.bdd}" disabled>
+                </div>
+                <!-- ====================================CHAMP BACKEND==================================== -->
+                <div class="col-sm-12 col-md-6 col-lg-6 mb-2">
+                  <label for="${idBdd}">Backend:</label>
+                  <input type="number" id="${idBackend}" class="form-control "  style="border: none;" value="${apprenant.backend}" disabled>
+                </div>
+                <!-- ====================================BOUTTON MODIFIER==================================== -->
+                <div class="text-end" id="${modifierApprenant}">
+                  <button id="modifier" class="btn"><i class="pe-3 bi bi-pen-fill modifierIcon"  title="Modifier"></i></button>
+                </div>
             </div>
-        </div>
 
-  </div>
-  </div>
+          </form>
+        </div>
+    </div>
+</div>
+</div>
 `) 
+let editInputNiveau = document.getElementById(idNiveau) 
 const updateBtn = document.getElementById(idUpdate)
 const deletBtn = document.getElementById(idDelete)
+const divCard = document.getElementById(card)
+const soumissionBtn = document.getElementById(modifierApprenant)
+editInputNiveau.value = apprenant.niveau
+soumissionBtn.style.display="none"
 updateBtn.addEventListener("click",()=>{
     APPRENANTS.forEach((apprenantItem)=>{
-        if (idUpdate.substring(7) == apprenant.index) {
-            prenomInput.value =apprenant.prenom
-            nomInput.value = apprenant.nom
-            niveauInput.value =apprenant.niveau
-            bioInput.value =apprenant.bio
-            maquettageInput.value = apprenant.maquettage
-            interfaceInput.value = apprenant.interface
-            backendInput.value = apprenant.backend
-            bddInput.value = apprenant.bdd
-            ajouterBtn.classList.add("d-none")
-            modifierBtn.classList.remove("d-none")           
+        if (idUpdate.substring(7) == apprenantItem.index) {
+            soumissionBtn.style.display="block"
+            let editInputPrenom = document.getElementById(idPrenom) 
+            let editInputNom = document.getElementById(idNom) 
+            let editInputBio = document.getElementById(idBio) 
+            let editInputMaquette = document.getElementById(idMaquette) 
+            let editInputBdd = document.getElementById(idBdd) 
+            let editInputInterface = document.getElementById(idInterface) 
+            let editInputBackend = document.getElementById(idBackend) 
+            let modifierApprenantForm = document.getElementById(idModifierApprenantForm)
+            editInputPrenom.removeAttribute('disabled')
+            editInputNom.removeAttribute('disabled')
+            editInputNiveau.removeAttribute('disabled')
+            editInputBio.removeAttribute('disabled')
+            editInputMaquette.removeAttribute('disabled')
+            editInputInterface.removeAttribute('disabled')
+            editInputBdd.removeAttribute('disabled')
+            editInputBackend.removeAttribute('disabled')
+            editInputPrenom.focus()
+          console.log(modifierApprenantForm);
+            modifierApprenantForm.addEventListener('submit',(e)=>{
+            e.preventDefault()
+            if (editInputPrenom.value.trim().length == 0) {
+              editInputPrenom.classList.add("invalid")
+              return
+            }
+            if (editInputNom.value.trim().length == 0) {
+              editInputNom.classList.add("invalid")
+              return
+            }
+  
+            if (editInputBio.value.trim().length <8) {
+                editInputBio.classList.add("invalid")
+                return
+            }
+            if (isNaN(editInputMaquette.value) || editInputMaquette.value < 0 ||  editInputMaquette.value >10 || editInputMaquette.value=="") {
+                editInputMaquette.classList.add("invalid")
+                return
+            }
+            if (isNaN(editInputInterface.value) || editInputInterface.value < 0 || editInputInterface.value >10 || editInputInterface.value=="") {
+              editInputInterface.classList.add("invalid")
+                return
+            }
+            if (isNaN(editInputBdd.value) || editInputBdd.value < 0 || editInputBdd.value >10 || editInputBdd.value=="") {
+              editInputBdd.classList.add("invalid")
+            }
+            if (isNaN(editInputBackend.value) || editInputBackend.value < 0 || editInputBackend.value >10 || editInputBackend.value=="") {
+              editInputBackend.classList.add("invalid")
+                return 
+            }
+            // CREATION DE L'OBJET APPRENANT MODIFIE
+            let updateApprenant = {
+              index: Date.now(),
+              prenom : editInputPrenom.value,
+              nom : editInputNom.value,
+              niveau : editInputNiveau.value,
+              bio : editInputBio.value,
+              photo : imgPreview.src,
+              maquettage : editInputMaquette.value,
+              interface : editInputInterface.value,
+              bdd: editInputBdd.value,
+              backend: editInputBackend.value
+            }
+            // AFFECTATION  DU NOUVEL APPRENANT
+            apprenantItem =updateApprenant
+          //  DESACTIVATION CHAMP
+          editInputPrenom.setAttribute('disabled',true)
+          editInputNom.setAttribute('disabled',true)
+          editInputNiveau.setAttribute('disabled',true)
+          editInputBio.setAttribute('disabled',true)
+          editInputMaquette.setAttribute('disabled',true)
+          editInputInterface.setAttribute('disabled',true)
+          editInputBdd.setAttribute('disabled',true)
+          editInputBackend.setAttribute('disabled',true)
+          soumissionBtn.style.display="none"
+          })
         }
+        
     })
 })
 }
+
 // AJOUT DES DONNES EN BASE
 saveCollectionBtn.addEventListener("click",()=>{
     APPRENANTS.forEach(apprenant=>{
@@ -177,6 +313,24 @@ saveCollectionBtn.addEventListener("click",()=>{
         })
     })
 })
+// MODIFICATION DU TABLEAU
+function modifier(carte,exApprenant) {
+    liste.removeChild(carte)            
+    let apprenantModifie ={
+        index: Date.now(),
+        prenom : prenomInput.value,
+        nom : nomInput.value,
+        niveau : niveauInput.value,
+        bio : bioInput.value,
+        photo : imgPreview.src,
+        maquettage : maquettageInput.value,
+        interface : interfaceInput.value,
+        bdd: bddInput.value,
+        backend: backendInput.value
+    }
+    exApprenant= apprenantModifie
+    createCard(exApprenant);
+}
 function resetInputs() {
     prenomInput.value=""
     nomInput.value =""
